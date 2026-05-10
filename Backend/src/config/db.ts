8c42a384597bcffc334623cbeb24ea8b91842944
MohamedEMonem <sim.mohamedessam44975@alexu.edu.eg>
2026-05-11 01:07:14 +0300

    refactor: simplify Prisma client initialization by removing adapter and connection string

--- Backend/src/config/db.ts
+++ Backend/src/config/db.ts
@@ -1,13 +1,5 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const connectionString = "postgresql://root:dokkanroot@dokkandb.c854csucuyms.us-east-1.rds.amazonaws.com:5432/postgres";
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not defined. Ensure .env is loaded before Prisma client initialization.");
-}
-
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;
