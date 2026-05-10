bfb1e0e66c9f675e5f16d9a04887a98f3a253233
MohamedEMonem <sim.mohamedessam44975@alexu.edu.eg>
2026-05-11 00:52:43 +0300

    add db string explicitly for testing purposes, will switch back to env variable in production.

--- Backend/src/config/db.ts
+++ Backend/src/config/db.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = "postgresql://root:dokkanroot@dokkandb.c854csucuyms.us-east-1.rds.amazonaws.com:5432/postgres";
 if (!connectionString) {
   throw new Error("DATABASE_URL is not defined. Ensure .env is loaded before Prisma client initialization.");
 }
